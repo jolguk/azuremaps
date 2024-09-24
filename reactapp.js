2abb92d963cb5621a11f5ef34092f719113db8a6
jolguk <jolguk@hotmail.co.uk>
2024-09-24 15:44:59 +0100

    Create reactapp.js

---
+++ reactapp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AzureMapsLoad from './AzureMapsLoad';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AzureMapsLoad />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
